4d4802b048aad22686ed83bded9891e044e6cb40
ponomarev.mike <ponomarev.mike@sharp-dev.net>
2017-03-13 13:53:37 +0500

    4 commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using log4net;
 
 namespace ConsoleApp
 {
     class Program
     {
-        private static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
