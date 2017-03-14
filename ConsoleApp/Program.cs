353e04de38e8973a57b209d3783e878683850854
ponomarev.mike <ponomarev.mike@sharp-dev.net>
2017-03-14 11:58:31 +0500

    10 comm

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            Console.WriteLine("Test!!!!");
             Console.ReadKey();
         }
     }
