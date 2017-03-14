45ae18146ecd856eae54667e52ff01cdcb65d725
ponomarev.mike <ponomarev.mike@sharp-dev.net>
2017-03-14 13:43:36 +0500

    10 commit

---
+++ ConsoleApp/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Service
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ UnitTests/ServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class ServiceTests
+    {
+        [Test]
+        public void SumTest()
+        {
+            Service service = new Service();
+            var expected = 10;
+
+            var actual = service.Sum(5, 5);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
